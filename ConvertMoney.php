1b8dc9769cbc9d7c27ca9729da86a0e96b4b067e
ahmadzaula <aamrimafaza@gmail.com>
2025-02-04 08:45:20 +0700

    diperbarui

--- ConvertMoney.php
+++ ConvertMoney.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulator dan Konverter Mata Uang</title>
+    <title>Konverter Mata Uang</title>
 </head>
     <link href="Kalkolator.css" rel="stylesheet">
 <body>
--- Kalkolator.php
+++ Kalkolator.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulator dan Konverter Mata Uang</title>
+    <title>Kalkulator</title>
 </head>
     <link href="Kalkolator.css" rel="stylesheet">
 <body>
